6cf5b3681c960791dd4ad6f49405aa10b7469c97
steelvelveteen <joeyvico@gmail.com>
2022-06-28 11:06:55 +0200

    Customer controller. Clean up

---
+++ BlogAPI/Controllers/CustomerController.cs
@@ -0,0 +1,26 @@
+using BlogServer.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogAPI.Controller;
+
+[ApiController]
+[Route("[controller]")]
+public class CustomerController : ControllerBase
+{
+	private readonly ApplicationDbContext _dbContext;
+	public CustomerController(ApplicationDbContext dbContext)
+	{
+		_dbContext = dbContext;
+
+	}
+
+	[HttpGet]
+	[Route("GetCustomers")]
+	public async Task<ActionResult<IEnumerable<CustomerController>>> Get()
+	{
+		var result = await _dbContext.Customers.ToListAsync();
+		return Ok(result);
+	}
+
+}
