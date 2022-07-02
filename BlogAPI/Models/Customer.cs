9660c01fb144cd5da5174b8a7f3d5b26a5847f11
steelvelveteen <joeyvico@gmail.com>
2022-07-02 16:40:30 +0200

    Added some data annotations to Customer model

--- BlogAPI/Models/Customer.cs
+++ BlogAPI/Models/Customer.cs
@@ -1,10 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BlogServer.Models;
 
 public class Customer
 {
-    public int Id { get; set;  }
-    public string FirstName { get; set; } = null!;
-    public string LastName { get; set; } = null!;
-    public string? Address { get; set; }
-    public string? Phone { get; set; }
+	public int Id { get; set; }
+
+	[Required]
+	public string FirstName { get; set; } = null!;
+
+	[Required]
+	public string LastName { get; set; } = null!;
+	public string? Address { get; set; }
+	public string? Phone { get; set; }
 }
