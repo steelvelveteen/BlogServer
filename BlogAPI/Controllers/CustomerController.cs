429b6f2d12877a8922018f07b16f8b4324c6651e
joeyOnMac <joeyvico@gmail.com>
2022-07-03 07:41:22 +0200

    Cleanup code

--- BlogAPI/Controllers/CustomerController.cs
+++ BlogAPI/Controllers/CustomerController.cs
@@ -92,10 +92,9 @@ public class CustomerController : ControllerBase
 	[ProducesResponseType(StatusCodes.Status404NotFound)]
 	public async Task<ActionResult> Delete([Required] int Id)
 	{
-		var customer = await _dbContext.Customers.FindAsync(Id);
+        var customer = await _dbContext.Customers.FindAsync(Id);
 		if (customer == null)
 		{
-
 			return NotFound("Customer not found.");
 		}
 		else
