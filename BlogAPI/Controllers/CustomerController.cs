c8e23fbee9f234d600b0df939c3725c9b228cc85
joeyOnMac <joeyvico@gmail.com>
2022-07-13 22:59:36 +0200

    Added badrequest returns if Id is not supplied for getcustomerby id and delete

--- BlogAPI/Controllers/CustomerController.cs
+++ BlogAPI/Controllers/CustomerController.cs
@@ -42,14 +42,18 @@ public class CustomerController : ControllerBase
 	/// Fetches a single customer
 	/// </summary>
 	/// <param name="Id">The Id of the customer we want to fetch</param>
+	/// <response code="400">If Id is not supplied</response>
 	/// <response code="200">Returns the customer dto</response>
 	/// <response code="404">If customer is not found in database</response>
 	[HttpGet("{Id}", Name = "GetCustomerById")]
+	[ProducesResponseType(StatusCodes.Status400BadRequest)]
 	[ProducesResponseType(StatusCodes.Status200OK)]
-	[ProducesResponseType(StatusCodes.Status404NotFound)]
-	public async Task<ActionResult<CustomerReadDto>> GetCustomerById(int Id)
+	[ProducesResponseType(typeof(CustomerReadDto),StatusCodes.Status404NotFound)]
+	public async Task<ActionResult<CustomerReadDto>> GetCustomerById([Required] int Id)
 	{
-		var customer = await _repository.GetCustomerById(Id);
+		if ( Id <= 0) return BadRequest();
+
+        var customer = await _repository.GetCustomerById(Id);
 
 		if (customer == null) return NotFound("Customer not found");
 
@@ -62,14 +66,18 @@ public class CustomerController : ControllerBase
 	/// </summary>
 	/// <param name="Id">The Id of the customer to delete from db.</param>
 	/// <returns>Ok</returns>
+	/// <response code="400">If Id is not supplied</response>
 	/// <response code="200">Returns ok containing string</response>
 	/// <response code="404">If the customer is not found in the db in the first place</response>
 	[HttpDelete("{Id}")]
+	[ProducesResponseType(StatusCodes.Status400BadRequest)]
 	[ProducesResponseType(StatusCodes.Status200OK)]
 	[ProducesResponseType(StatusCodes.Status404NotFound)]
 	public async Task<ActionResult> Delete([Required] int Id)
 	{
-		var customerInDb = await _repository.GetCustomerById(Id);
+		if (Id <= 0) return BadRequest();
+
+        var customerInDb = await _repository.GetCustomerById(Id);
 
 		if (customerInDb == null)
 		{
