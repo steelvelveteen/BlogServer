cdd74a9b1e859d2113d038eaa0d184cd7b08907e
steelvelveteen <joeyvico@gmail.com>
2022-07-15 11:35:05 +0200

    2 unit tests but cannot figure out how to get the actual value from the result

--- BlogAPI.Tests/CustomerControllerTests.cs
+++ BlogAPI.Tests/CustomerControllerTests.cs
@@ -3,20 +3,26 @@ using BlogAPI.Repository;
 using AutoMapper;
 using BlogAPI.Controllers;
 using Microsoft.AspNetCore.Mvc;
+using BlogAPI.Models;
+using BlogAPI.DTOs;
+using System.Net;
 
 namespace BlogAPI.Tests;
 
 public class CustomerControllerTests
 {
+	private readonly Mock<IMapper> mapperStub = new();
+	private readonly Mock<ICustomerRepository> repositoryStub = new();
+	private readonly Mock<CustomerController> controller = new();
+
 	[Fact]
 	// Naming convention: unitOfWork_stateUnderTest_expectedBehaviour
 	public async Task GetCustomerById_withUnexistingCustomerId_ReturnsNotFound()
 	{
 		// Arrange
-		var repositoryStub = new Mock<ICustomerRepository>();
-		repositoryStub.Setup(repo => repo.GetCustomerById(It.IsAny<int>()));
-
-		var mapperStub = new Mock<IMapper>();
+		repositoryStub
+		.Setup(repo => repo.GetCustomerById(It.IsAny<int>()))
+		.ReturnsAsync((Customer?)null);
 
 		var controller = new CustomerController(mapperStub.Object, repositoryStub.Object);
 
@@ -25,5 +31,34 @@ public class CustomerControllerTests
 
 		// Assert
 		Assert.IsType<NotFoundObjectResult>(result.Result);
+	}
+
+	[Fact]
+	public async Task GetCustomerById_WithExistingCustomer_ReturnsOk()
+	{
+		// Arrange
+		var customer = new Customer
+		{
+			Id = 1,
+			FirstName = "Test",
+			LastName = "Last Test",
+			Address = "",
+			Phone = "8994545112"
+		};
+
+		repositoryStub
+		.Setup(repo => repo.GetCustomerById(It.IsAny<int>()))
+		.ReturnsAsync(customer);
+
+		var controller = new CustomerController(mapperStub.Object, repositoryStub.Object);
+
+		// Act
+		var result = await controller.GetCustomerById(1);
+		var okObjResult = result.Result;
+
+		// Assert
+		Assert.IsType<OkObjectResult>(okObjResult);
+
+		// Assert.Equal(HttpStatusCode.OK, result.Result);
 	}
 }
