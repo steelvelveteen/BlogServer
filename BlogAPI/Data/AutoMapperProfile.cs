99e358177e0439b24e111cf64d4ffb8c9e078ea4
steelvelveteen <joeyvico@gmail.com>
2022-07-16 20:07:04 +0200

    Merged develop

--- BlogAPI/Data/AutoMapperProfile.cs
+++ BlogAPI/Data/AutoMapperProfile.cs
@@ -19,8 +19,6 @@ public class AutoMapperProfile : Profile
 		// CreateMap<EditFlightModel, Flight>()
 		//   .ForAllMembers(opts => opts.Condition((src, //dest, srcMember) => srcMember != null));
 
-		// CreateMap<PaymentSourceRequest, Braintree.PaymentMethodRequest>()
-		// ForMember(d => d.Id, opt => opt.Ignore())
 		//   .ForMember(dest => dest.PaymentMethodNonce, opt => opt.MapFrom(src => src.Nonce));
 	}
 }
